58dbe1db04ceb1495666cf9df1cb138650e86e5a
ENRIQUE MARTINEZ <enriquemartinezenriquem@outlook.com>
2022-05-09 18:48:03 -0500

    avance desarrollo

---
+++ src/actores/FormularioActores.tsx
@@ -0,0 +1,32 @@
+import { Form, Formik, FormikHelpers, yupToFormErrors } from "formik";
+import Button from "../utils/Button";
+import FormGroupText from "../utils/FormGroupText";
+import { actorCreacionDTO } from "./actores.model";
+import { Link } from "react-router-dom";
+import * as Yup from 'yup';
+
+export default function FormularioActores(props: formularioACtoresProps){
+    return(
+        <Formik
+            initialValues={props.modelo}
+            onSubmit={props.onSubmit}>
+                validationSchema={Yup.object({
+                    nombre: Yup.string().required('este campo es requerido').primeraLetraMayuscula()
+                })}
+            {(formikProps) => (
+                <Form>
+                    <FormGroupText campo="nombre" label="Nombre"/>
+                    <Button disabled={formikProps.isSubmitting}
+                        type="submit"
+                    >Salvar</Button>
+                    <Link className="btn btn-secondary" to="/actores">Cancelar</Link>
+                </Form>
+            )}    
+        </Formik>
+    )
+}
+
+interface formularioACtoresProps{
+    modelo: actorCreacionDTO;
+    onSubmit(valores: actorCreacionDTO, acciones: FormikHelpers<actorCreacionDTO>): void;
+}
--- src/utils/Button.tsx
+++ src/utils/Button.tsx
@@ -3,7 +3,7 @@
 
 export default function Button(props: buttonProps) {
     return (
-        <button type={props.type} className="btn btn-primary"
+        <button type={props.type} className={props.className}
         disabled={props.disabled}
           onClick={props.onClick}
         >{props.children}</button>
@@ -15,9 +15,11 @@ interface buttonProps{
     onClick?(): void;
     type: "button" | "submit";
     disabled: boolean;
+    className: string;
 }
 
 Button.defaultProps = {
     type: "button",
-    disabled: false
+    disabled: false,
+    className: "btn btn-primary"
 }
